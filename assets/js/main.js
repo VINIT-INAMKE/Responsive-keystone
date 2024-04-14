1a2ff0596f1cc5791f03d4c10baf89807d98f2cf
VINIT-INAMKE <vinray0419gaming@gmail.com>
2024-04-14 15:02:31 +0530

    14-04-2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,32 @@ const removeStyle = () => {
 };
 
 addEventListener("resize", removeStyle);
+
+/*=============== Association slider STYLES ===============*/
+const btns = document.querySelectorAll(".assnav-btn");
+const slides = document.querySelectorAll(".assimg-slide");
+const contents = document.querySelectorAll(".asscontent");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
